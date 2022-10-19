715449b1dac29e39635c332baa5684eb653440f1
Germán Oyarzo <71647622+germanoyarzo@users.noreply.github.com>
2022-10-19 16:55:53 -0300

    Delete demospringboots3 directory

--- demospringboots3/src/main/java/com/example/demospringboots3/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demospringboots3.repository;
-
-import com.example.demospringboots3.model.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
-}
